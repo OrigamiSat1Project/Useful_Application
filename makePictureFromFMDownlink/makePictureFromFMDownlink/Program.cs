d6188c8ee6d72888da1ba92dad95e3b8545b8dcb
reokashiyama <31460945+reokashiyama@users.noreply.github.com>
2018-03-20 05:20:16 +0900

    Update Program.cs - file deaing using byte[]

--- makePictureFromFMDownlink/makePictureFromFMDownlink/Program.cs
+++ makePictureFromFMDownlink/makePictureFromFMDownlink/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +13,10 @@ namespace makePictureFromFMDownlink
         static void Main(string[] args)
         {
             char c = Path.DirectorySeparatorChar;
-            string separater = "JQ1YCZ>JQ1ZHX:";
+            string separater_header = "JQ1YCZ>JQ1ZHX:";
+            string separater_footer = "\r\n";
+            int actual_packet_length = 32;
+            int including_other_packet_length = separater_header.Length + actual_packet_length + separater_footer.Length;
             string footer = "\r\n";
             if (args.Length != 1)
             {
@@ -27,37 +31,52 @@ namespace makePictureFromFMDownlink
             }
 
             Show("Now file open then read...");
-            StreamReader sr = new StreamReader(downlink_data_file_path, Encoding.Default);
-            string raw_data = sr.ReadToEnd();
-            string edited_data = string.Empty;
-            var packet_list = raw_data.Split(new string[] { separater }, StringSplitOptions.None);
-            int raw_lines = packet_list.Length;
+            FileStream fs = new FileStream(downlink_data_file_path, FileMode.Open);
 
-            int index = 0;
-            int lines = 0;
-            string pk_already_get = string.Empty;
-            foreach (var pk in packet_list)
+            byte[] raw_data = new byte[fs.Length + (including_other_packet_length - fs.Length % including_other_packet_length)];
+            fs.Read(raw_data, 0, raw_data.Length);
+            fs.Close();
+            Show("Now finish reading then close file...");
+            string edited_data = string.Empty;
+            //var packet_list = raw_data.Split(new string[] { separater }, StringSplitOptions.None);
+            List<byte[]> packet_list = new List<byte[]>();
+            int count = 0;
+            byte[] pk_previous = new byte[actual_packet_length];
+            int duplicate_count = 0;
+            for (count = 0; count < (int)(raw_data.Length / including_other_packet_length); count++)
             {
-                if (pk != pk_already_get)
+                bool duplicate = true;
+                int pk_index = including_other_packet_length * count;
+                byte[] including_other_pk = new byte[including_other_packet_length];
+                Buffer.BlockCopy(raw_data, pk_index, including_other_pk, 0, including_other_packet_length);
+                byte[] pk = new byte[actual_packet_length];
+                Buffer.BlockCopy(including_other_pk, separater_header.Length, pk, 0, actual_packet_length);
+
+                for (int i = 0; i < actual_packet_length; i++)
                 {
-                    lines++;
-                    string edited_line = pk.Substring(0, pk.Length - 2);
-                    //int index_last = pk.Length - 1;
-                    //if (pk.Substring(index_last, 1) == "\n")
-                    //{
-                    //    pk.Remove(index_last, 1);
-                    //}
-                    //index_last = pk.Length - 1;
-                    //if (pk.Substring(index_last, 1) == "\r")
-                    //{
-                    //    pk.Remove(index_last, 1);
-                    //}
-                    edited_data += edited_line;
-                    Show(lines + " lines already added.");
+                    if (pk[i] != pk_previous[i])
+                    {
+                        duplicate = false;
+                    }
                 }
-                pk_already_get = pk;
+                if (!duplicate)
+                {
+                    packet_list.Add(pk);
+                    Buffer.BlockCopy(pk, 0, pk_previous, 0, actual_packet_length);
+                }
+                else
+                {
+                    duplicate_count++;
+                }
             }
-            Show(lines + " lines is added. " + (raw_lines - lines) + " lines is duplicate...");
+
+            int actual_count = count - duplicate_count;
+            byte[] actual_packets = new byte[actual_count * actual_packet_length];
+            for (int i = 0; i < actual_count; i++)
+            {
+                Buffer.BlockCopy(packet_list[i], 0, actual_packets, actual_packet_length*i, actual_packet_length);
+            }
+            Show(actual_count + " / " + count + " packets is added. " + duplicate_count + " / " + count + " packets is duplicate...");
             Show("Now file open then write...");
 
             string edited_file_path = Path.GetDirectoryName(downlink_data_file_path) + c + Path.GetFileNameWithoutExtension(downlink_data_file_path) + "_edited_" + DateTime.Now.ToString("yyyyMMddhhss") + Path.GetExtension(downlink_data_file_path);
@@ -65,15 +84,15 @@ namespace makePictureFromFMDownlink
             {
                 File.Delete(edited_file_path);
             }
-            StreamWriter sw = new StreamWriter(edited_file_path, false, Encoding.Default);
-            sw.Write(edited_data);
-            Show("Now complete, new file path is [" + edited_file_path + "].");
-            Show("If you want to see as jpeg, please change extension to \".jpg\".");
+            FileStream _fs = new System.IO.FileStream(edited_file_path, FileMode.CreateNew, FileAccess.Write);
+            _fs.Write(actual_packets, 0, actual_packets.Length);
+            _fs.Close();
+            Show("Now complete, new file path is [ " + edited_file_path + " ].");
         }
 
         static public void Show(string message)
         {
-            Console.WriteLine(message);
+            Console.WriteLine(Path.GetFileName(Assembly.GetExecutingAssembly().Location) + "\t" + message);
         }
     }
 
